9b164c9abba7d3d82ca9d6211994ab66a07310dd
Iain MacLeod <iain.x.macleod@i.tescobank.com>
2017-04-08 23:04:09 +0100

    split flow transformation to make prime factors of 4 case pass

--- primefactors/primefactors.go
+++ primefactors/primefactors.go
@@ -3,7 +3,13 @@ package katas
 func primeFactorsOf(n int) []int {
 	var factors []int
 	if n > 1 {
-		factors = append(factors, 2)
+		if n%2 == 0 {
+			factors = append(factors, 2)
+			n /= 2
+		}
+		if n > 1 {
+			factors = append(factors, n)
+		}
 	}
 	return factors
 }
--- primefactors/primefactors_test.go
+++ primefactors/primefactors_test.go
@@ -17,3 +17,19 @@ func TestSumOfPrimeFactorsOfTwo(t *testing.T) {
 		t.Fatalf("Expected %d but got %d", expected, actual)
 	}
 }
+
+func TestSumOfPrimeFactorsOfThree(t *testing.T) {
+	actual := primeFactorsOf(3)
+	expected := []int{3}
+	if !sliceCompare(expected, actual) {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestSumOfPrimeFactorsOfFour(t *testing.T) {
+	actual := primeFactorsOf(4)
+	expected := []int{2, 2}
+	if !sliceCompare(expected, actual) {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
